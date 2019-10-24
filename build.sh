51d32209137d1ba1da83408d9d8805989c58f341
shzy2012 <shzy2012@gmail.com>
2019-10-24 13:48:24 +0800

    delete build.sh

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo start building
-
-env GOOS=linux GOARCH=amd64 go build -mod=vendor -o bot_msg_console main.go
-scp bot_msg_console root@39.96.21.121:/home/works/zhouyi/bot_msg_console
-
-rm -f bot_msg_console 
-echo build finished
